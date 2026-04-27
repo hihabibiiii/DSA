8b1374a2556b032eeb210fe7eb2684cd4556dcb4
Habib <hshabibullah@gmail.com>
2026-04-27 16:45:05 +0530

    initial Commit

---
+++ removeDublicate_and_Zero_space.py
@@ -0,0 +1,20 @@
+# Find Sum Of Two Number With Two Pointer
+
+class soluation:
+    def TwoNumSum(self, number, target):
+        i,j = 0, len(number) - 1
+
+        while i < j:
+            s = number[i] + number[j]
+            if s == target:
+                return (i +1, j + 1)
+            elif s < target:
+                i += 1
+            else:
+                j -= 1
+
+obj = soluation()
+nums = [2,3,4,5,6,7,8,9]
+target = 13
+result = obj.TwoNumSum(nums, target)
+print(result)
---
+++ test.py
@@ -0,0 +1,13 @@
+
+def twoSum(  numbers, target):
+    i, j = 0 , len(numbers) - 1
+
+    while i < j:
+        s = numbers[i] + numbers[j]
+        if s == target:
+            return i + 1 , j + 1
+        elif s < target:
+            i += 1
+        else:
+            j -= 1
+    
